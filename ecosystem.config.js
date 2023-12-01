d506119df9092705f61c56dba51d95661e101c29
Dmitry <mityourik@gmail.com>
2023-12-01 09:25:09 -0300

    change ipv4 for ecosystem2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       repo: 'https://github.com/mityourik/movies-explorer-api',
       path: '/home/mityourik/auto-deploy-api',
       'pre-deploy-local':
-      'scp .env mityourik@51.250.11.32:/home/mityourik/auto-deploy-api/current/',
+      'scp .env mityourik@158.160.121.117:/home/mityourik/auto-deploy-api/current/',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
     },
   },
