1096f0d37fe2cb2bebcb901ae1642b7730639e1a
Dmitry <mityourik@gmail.com>
2023-12-04 11:12:55 -0300

    remove hex for id validation)

--- validationSchemas/joiValidationSchemas.js
+++ validationSchemas/joiValidationSchemas.js
@@ -42,7 +42,7 @@ const createMovieSchema = celebrate({
 
 const deleteMovieSchema = celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().hex().required(),
+    movieId: Joi.number().required(),
   }),
 });
 
